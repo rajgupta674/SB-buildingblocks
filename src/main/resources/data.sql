f81180fab62bb1cab0a8e29152195c0eaf23fc11
rajgu <rajgupta674@gmail.com>
2020-03-31 13:34:06 +0530

    JPA Base RestServices Development Completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101, 'rajkumar@stacksimplify.com', 'Rajkumar', 'Gupta', 'admin', 'ssn101', 'rajgupta');
+insert into user values(102, 'ashis@stacksimplify.com', 'Ashish', 'Kumar', 'admin', 'ssn102', 'krashis');
+insert into user values(103, 'pawan@stacksimplify.com', 'Pawan', 'Singh', 'admin', 'ssn103', 'psingh');    
